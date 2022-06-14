f0aa5b39d5eb76267e7f36f2b744bb6308f73ece
tenchooooo <tenchooo.227@gmail.com>
2022-06-14 08:10:06 +0000

    address完了

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -5,27 +5,31 @@ class Public::AddressesController < ApplicationController
   end
 
   def edit
-
+    @address = Address.find(params[:id])
   end
 
   def create
-    @address_new = Address.new(address_params)
+    @address = Address.new(address_params)
     @address.save
     redirect_to addresses_path
   end
 
   def update
-
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
-
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
   end
 
   private
 
   def address_params
-    params.require(:address).permit(:name, :postal_code, :address)
+    params.require(:address).permit(:name, :postal_code, :address, :customer_id)
   end
 
 end
