8b0555d54b90b45ab77f352aa3889152afdc865e
tenchooooo <tenchooo.227@gmail.com>
2022-06-14 14:56:48 +0000

    order_new完了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,25 +2,29 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
   end
-  
+
   def confirm
   end
-  
+
   def complete
   end
-  
+
   def create
-    @oeder = Order.new(oeder_params)
+    @oeder = Order.new(order_params)
     @oeder.save
     redirect_to confirm_orders_path
   end
-  
+
   def index
   end
-  
+
   def show
   end
-  
+
   private
-  
+
+  def order_params
+     params.require(:order).permit(:postal_code, :address, :name, :shpping_cost, :total_payment, :payment_method, :status, :customer_id)
+  end
+
 end
